72ab6e8f760a463fa1859f6bb8ee324de6cee43e
Tom <tomredway1@gmail.com>
2025-10-31 16:51:10 +0000

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,21 @@ import Uploader from './components/Uploader'
 function App() {
   const [count, setCount] = useState(0)
 
+  const apiRequest = (file) => {
+
+    fetch("http://localhost:8000/api/analyse", {
+      method: "POST",
+      body: JSON.stringify({
+        ppt: file
+      }),
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
+
   return <Uploader />
 }
 
